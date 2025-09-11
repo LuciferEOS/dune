3f7fb237529c8aa0c3eeaedab61cadfa0679f833
LuciferEOS <stepanteliatnik2022@gmail.com>
2025-09-11 17:33:26 +0200

    dunemodules

---
+++ Content.Dune.Server/EntryPoint.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.ContentPack;
+
+namespace Content.Dune.Server;
+
+public sealed class DuneServerEntryPoint : GameServer
+{
+    public override void PreInit() { }
+    public override void Init() { }
+    public override void PostInit() { }
+}
